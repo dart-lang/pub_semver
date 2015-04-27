f92d88b944e98032a27c692569402c91c07664f4
Kevin Moore <kevmoo@google.com>
2015-04-27 09:01:20 -0700

    remove analyzer warnings

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -122,7 +122,7 @@ class Version implements Comparable<Version>, VersionConstraint {
       String build = match[8];
 
       return new Version._(major, minor, patch, preRelease, build, text);
-    } on FormatException catch (ex) {
+    } on FormatException {
       throw new FormatException('Could not parse "$text".');
     }
   }
@@ -150,7 +150,7 @@ class Version implements Comparable<Version>, VersionConstraint {
     return text.split('.').map((part) {
       try {
         return int.parse(part);
-      } on FormatException catch (ex) {
+      } on FormatException {
         // Not a number.
         return part;
       }
